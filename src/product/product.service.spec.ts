0684dcc0b2be6c2156de929dde5c13893b19b56c
Manuel Torres Gil <mtorres@ual.es>
2021-11-11 21:03:25 +0100

    Product service unit test - v0

--- src/product/product.service.spec.ts
+++ src/product/product.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductService } from './product.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
 
 describe('ProductService', () => {
   let service: ProductService;
+  let mockProductsRepository = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductService],
+      providers: [
+        ProductService,
+        {
+          provide: getRepositoryToken(Product),
+          useValue: mockProductsRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<ProductService>(ProductService);
